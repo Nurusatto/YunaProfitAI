e4d2ab26e321248cd6c7ba06872131e05f56ada8
Nurusatto <nursat21102006@gmail.com>
2025-11-16 23:42:06 +0500

    start

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      generatedRouteTree: "./src/routes/routeTree.gen.ts",
+    }),
+    react(),
+  ],
+});
